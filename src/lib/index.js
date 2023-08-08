6852df722d497d771b78cfa0767deba2359c3850
Garik Asplund <garik.asplund@gmail.com>
2023-08-07 19:42:56 -0700

    basic layout

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,52 @@
 // place files you want to import through the `$lib` alias in this folder.
+const letters = [
+    "B",
+    "C",
+    "D",
+    "F",
+    "G",
+    "H",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "P",
+    "R",
+    "S",
+    "T",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+];
+
+const targets = [
+    "D",
+    "F",
+    "R",
+    "X",
+];
+
+const distractors = [
+    [0,0,255],      // blue
+    [200,200,0],    // yellow
+    [200,0,200],    // magenta
+    [0,200,200],    // cyan
+    [200,200,200],  // light grey
+    [50,50,50],     // dark grey
+];
+
+// 16 items per array
+// place target letter at _ index
+
+// start 400ms blank
+
+// 50ms per frame
+// alternating letter, blank frame
+
+// T1/T2 lag either 300ms or 800ms
+
+// reverse responses are correct
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,24 @@
+<script>
+
+</script>
+
 <h1 class="transform translate-y-20 flex justify-center text-4xl font-bold underline">
     Welcome to our experiment :)
 </h1>
+
+<div class="flex flex-col items-center">
+    <h3 class="text-center transform translate-y-40 text-xl w-2/5">
+        Whenever you see a red letter, press that key on your keyboard
+    </h3>
+
+    <button on:click={begin} class="bg-gray-100 p-2 border rounded-sm border-black transform translate-y-60 text-4xl">
+        Start
+    </button>
+
+    <div class="transform translate-y-80">
+        <p>place letters here!!</p>
+    </div>
+</div>
 
 <style lang="postcss">
     :global(html) {
