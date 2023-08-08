8afd0b9890d558685be75bb36dcef16dcc586dc3
Garik Asplund <garik.asplund@gmail.com>
2023-08-07 18:36:50 -0700

    making home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="transform translate-y-20 flex justify-center text-4xl font-bold underline">
+    Welcome to our experiment :)
+</h1>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme(colors.fuchsia.200);
+    }
+</style>    
