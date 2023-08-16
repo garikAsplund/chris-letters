bee3c0d04c46cd748e3c4c50de96988d752dcae3
Garik Asplund <garik.asplund@gmail.com>
2023-08-16 11:36:10 -0700

    try this again

---
+++ src/lib/components/AccuracyBar.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let correct;
+    export let attempts;
+  
+    $: accuracy = (correct / attempts) * 100;
+</script>
+
+<div class="w-full h-3 bg-red-500 ">
+    <div class="h-3 bg-green-500 accuracy-inner" style="width: {accuracy}%"></div>
+</div>
+
+<style>
+    .accuracy-inner {
+      transition: width 0.5s ease-in-out;
+    }
+</style>
