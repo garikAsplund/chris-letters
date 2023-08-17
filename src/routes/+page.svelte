bc6f08d986113b58d52459abf654ca902e3710ac
Garik Asplund <garik.asplund@gmail.com>
2023-08-16 17:56:27 -0700

    added firebase and google auth

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,55 @@
 	import AccuracyBar from '$lib/components/AccuracyBar.svelte';
     import ProgressBar from '$lib/components/ProgressBar.svelte';
     import { emojisplosion } from 'emojisplosion';
+
+    // Import the functions you need from the SDKs you need
+    import { initializeApp } from "firebase/app";
+    import { getDatabase } from "firebase/database";
+    import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut } from "firebase/auth";
+
+    // TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
+
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const firebaseConfig = {
+        apiKey: "AIzaSyDiauRrelcJu1GuYZ5Fqz6cz2y0LLv3C3g",
+        authDomain: "chris-letters.firebaseapp.com",
+        projectId: "chris-letters",
+        storageBucket: "chris-letters.appspot.com",
+        messagingSenderId: "867592075139",
+        appId: "1:867592075139:web:bc8cfdcd4d74bd0a2668b6",
+        measurementId: "G-3SEHN30EF8"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const database = getDatabase(app);
+    const auth = getAuth(app);
+
+    const googleProvider = new GoogleAuthProvider();
+
+    async function signInWithGoogle() {
+        try {
+            const result = await signInWithPopup(auth, googleProvider);
+            const user = result.user;
+        } catch (error) {
+            console.error("Error signing in with Google:", error.message);
+        }
+    }
+
+    let user = null;
+    onAuthStateChanged(auth, (currentUser) => {
+        user = currentUser;
+    });
+
+    async function handleSignOut() {
+        try {
+        await signOut(auth);
+        } catch (error) {
+        console.error("Error signing out:", error.message);
+        }
+    }
     
     const letters = [
         "B",
@@ -213,6 +262,7 @@
         if (SiB) {
             if (surpriseTrials.includes(currentTrial) && (count === targetIndex * 2 - 6 || count === targetIndex * 2 - 5)) {
                 displayFace = true;
+                console.log(displayFace);
             } else if (count === targetIndex * 2) {
                 displayFace = false;
                 currentLetter = targetLetter;
@@ -321,114 +371,129 @@
 <svelte:window on:keydown={handleKeydown} on:keyup={nextTrial}/>
 
 <html lang="en" class="h-screen bg-no-repeat bg-gradient-to-b from-fuchsia-300 to-white">
-    {#if currentTrial < 96}
-        <h1 class="flex justify-center text-4xl font-bold text-center transform translate-y-10">
-            🪇 Welcome to our experiment 🧑‍🔬
-        </h1>
-        
-        <div class="flex justify-center mx-4 space-x-4 translate-y-24">      
-            <label>
-                <input 
-                    type=checkbox
-                    value="AB"
-                    on:input={handleCheck}
-                    bind:checked={AB} 
-                />
-                Attentional blink
-            </label>
-            <label>
-                <input 
-                    type=checkbox
-                    value="CC"
-                    on:input={handleCheck}
-                    bind:checked={CC}
-                />
-                Contingent capture
-            </label>
-            <label>
-                <input 
-                    type=checkbox
-                    value="SiB"
-                    on:input={handleCheck}
-                    bind:checked={SiB}
-                />
-                Surprise-induced blindness
-            </label>
-        </div>
+<body>  
+    {#if !user}
+    <div class="flex flex-col items-center justify-center h-screen space-y-10">
+        <button class="px-4 py-2 font-semibold text-gray-600 bg-transparent border border-gray-500 rounded hover:bg-gray-500 hover:text-white hover:border-transparent" on:click={signInWithGoogle}>
+            Sign in with Google
+        </button> 
+    </div>
+    {:else}
+        {#if currentTrial < 96}
+            <h1 class="flex justify-center text-4xl font-bold text-center transform translate-y-10">
+                🪇 Welcome to our experiment 🧑‍🔬
+            </h1>
+            
+            <div class="flex justify-center mx-4 space-x-4 translate-y-24">      
+                <label>
+                    <input 
+                        type=checkbox
+                        value="AB"
+                        on:input={handleCheck}
+                        bind:checked={AB} 
+                    />
+                    Attentional blink
+                </label>
+                <label>
+                    <input 
+                        type=checkbox
+                        value="CC"
+                        on:input={handleCheck}
+                        bind:checked={CC}
+                    />
+                    Contingent capture
+                </label>
+                <label>
+                    <input 
+                        type=checkbox
+                        value="SiB"
+                        on:input={handleCheck}
+                        bind:checked={SiB}
+                    />
+                    Surprise-induced blindness
+                </label>
+            </div>
 
-        <div class="flex flex-col items-center">
-            {#if !AB && !CC && !SiB}
-                <p class="p-2 text-4xl translate-y-32">Please select an option from above 👆</p>
-            {:else if !clicked}
-                <button on:click={onClick} class="p-2 text-4xl transform translate-y-32 bg-gray-100 border border-black rounded-sm hover:bg-gray-200">
-                    {buttonText}
-                </button>
-            {:else}
-                <p class="p-2 text-4xl text-transparent translate-y-32">Shhh, this is secret!!!</p>
-            {/if}
-            <h3 class="w-2/5 text-xl text-center transform translate-y-40">
-                Whenever you see a red letter, press that key on your keyboard
-            </h3>
-            <!-- <p class="self-center text-lg text-center translate-y-56">Then hit spacebar to start the next trial</p> -->
+            <div class="flex flex-col items-center">
+                {#if !AB && !CC && !SiB}
+                    <p class="p-2 text-4xl translate-y-32">Please select an option from above 👆</p>
+                {:else if !clicked}
+                    <button on:click={onClick} class="p-2 text-4xl transform translate-y-32 bg-gray-100 border border-black rounded-sm hover:bg-gray-200">
+                        {buttonText}
+                    </button>
+                {:else}
+                    <p class="p-2 text-4xl text-transparent translate-y-32">Shhh, this is secret!!!</p>
+                {/if}
+                <h3 class="w-2/5 text-xl text-center transform translate-y-40">
+                    Whenever you see a red letter, press that key on your keyboard
+                </h3>
+                <!-- <p class="self-center text-lg text-center translate-y-56">Then hit spacebar to start the next trial</p> -->
 
-            <div class="flex justify-center w-64 h-64 transform translate-y-48 border-8" class:border-red-500={isBoxRed} class:border-green-500={isBoxGreen} class:border-slate-500={!isBoxGreen && !isBoxRed}>
-                <p class="self-center font-thin text-center text-[280px] font-courier-new" class:text-red-500={isTarget} style="color: {textColor}">
-                    {#if displayFace}
-                        <img src="garik_bw.jpg" alt="Garik!!!">
+                <div class="flex justify-center w-64 h-64 transform translate-y-48 border-8" class:border-red-500={isBoxRed} class:border-green-500={isBoxGreen} class:border-slate-500={!isBoxGreen && !isBoxRed}>
+                    <p class="self-center font-thin text-center text-[280px] font-courier-new" class:text-red-500={isTarget} style="color: {textColor}">
+                        {#if displayFace}
+                            <img src="garik_bw.jpg" alt="Garik!!!">
+                        {:else}
+                            {currentLetter}
+                        {/if}
+                    </p>
+                </div>
+                <p class="self-center text-center translate-y-56 text">
+                    {#if AB}
+                        Your guesses:
                     {:else}
-                        {currentLetter}
+                        Your guess:
                     {/if}
                 </p>
+                <p class="self-center h-24 font-thin text-center translate-y-56 text-8xl font-courier-new">
+                    {#if AB}
+                        {#each guesses as guess}
+                            {guess}
+                        {/each}
+                    {:else}
+                        {receivedLetter}
+                    {/if}
+                </p>
+
+                <p class="self-center text-center translate-y-56 text">Your reaction time:</p>
+                <p class="self-center h-24 text-2xl font-thin text-center translate-y-56 font-courier-new">{reactionTime} ms</p>
+
+                <div class="fixed bottom-0 left-0 w-full">
+                    {#if user}
+                    <div class="flex justify-center">
+                        <button on:click={handleSignOut}>Sign Out</button>
+                    </div>
+                    {/if}
+                    <ProgressBar current={currentTrial} total={totalTrials}/>
+                    <AccuracyBar correct={correct} attempts={correct + incorrect}/>
+                </div>
             </div>
-            <p class="self-center text-center translate-y-56 text">
-                {#if AB}
-                    Your guesses:
-                {:else}
-                    Your guess:
-                {/if}
-            </p>
-            <p class="self-center h-24 font-thin text-center translate-y-56 text-8xl font-courier-new">
-                {#if AB}
-                    {#each guesses as guess}
-                        {guess}
-                    {/each}
-                {:else}
-                    {receivedLetter}
-                {/if}
-            </p>
 
-            <p class="self-center text-center translate-y-56 text">Your reaction time:</p>
-            <p class="self-center h-24 text-2xl font-thin text-center translate-y-56 font-courier-new">{reactionTime} ms</p>
-
-            <div class="fixed bottom-0 left-0 w-full">
-                <ProgressBar current={currentTrial} total={totalTrials}/>
-                <AccuracyBar correct={correct} attempts={correct + incorrect}/>
-            </div>
-        </div>
-
-    {:else}
-        <h1 class="flex justify-center text-4xl font-bold transform translate-y-20 ">
-            😍 Thanks for playing!!! 😎
-        </h1>
-        
-        {#if (correct / (correct + incorrect)) > 0.8}
-            <h1 class="flex justify-center text-2xl font-bold transform translate-y-32">
-                🫨 Wow, way to go!
-            </h1>
-        {:else if (correct / (correct + incorrect)) > 0.65}
-            <h1 class="flex justify-center text-2xl font-bold transform translate-y-32">
-                👏 You're doing alright.
-            </h1>
         {:else}
-            <h1 class="flex justify-center text-2xl font-bold transform translate-y-32">
-                🫣 Keep trying.
+            <h1 class="flex justify-center text-4xl font-bold transform translate-y-20 ">
+                😍 Thanks for playing!!! 😎
             </h1>
-        {/if}
+            
+            {#if (correct / (correct + incorrect)) > 0.8}
+                <h1 class="flex justify-center text-2xl font-bold transform translate-y-32">
+                    🫨 Wow, way to go!
+                </h1>
+            {:else if (correct / (correct + incorrect)) > 0.65}
+                <h1 class="flex justify-center text-2xl font-bold transform translate-y-32">
+                    👏 You're doing alright.
+                </h1>
+            {:else}
+                <h1 class="flex justify-center text-2xl font-bold transform translate-y-32">
+                    🫣 Keep trying.
+                </h1>
+            {/if}
 
-        <div class="flex flex-col items-center">
-            <h3 class="w-2/5 text-xl text-center transform translate-y-44">
-                See you next time.
-            </h3>
-        </div>
+            <div class="flex flex-col items-center">
+                <h3 class="w-2/5 text-xl text-center transform translate-y-44">
+                    See you next time.
+                </h3>
+            </div>
+        {/if}
     {/if}
+</body>
 </html>
