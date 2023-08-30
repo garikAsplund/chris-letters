5a441c4a3bef4f8ad47151a846099b68f6241251
Garik Asplund <garik.asplund@gmail.com>
2023-08-30 14:40:12 -0700

    .env baby

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,14 +1,25 @@
 import { initializeApp } from "firebase/app";
 
+const {
+    VITE_FIREBASE_API_KEY,
+    VITE_FIREBASE_AUTH_DOMAIN,
+    VITE_FIREBASE_DATABASE_URL,
+    VITE_FIREBASE_PROJECT_ID,
+    VITE_FIREBASE_STORAGE_BUCKET,
+    VITE_FIREBASE_MESSAGING_SENDER_ID,
+    VITE_FIREBASE_APP_ID,
+    VITE_FIREBASE_MEASUREMENT_ID
+} = import.meta.env;
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDiauRrelcJu1GuYZ5Fqz6cz2y0LLv3C3g",
-    authDomain: "chris-letters.firebaseapp.com",
-    databaseURL: "https://chris-letters-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "chris-letters",
-    storageBucket: "chris-letters.appspot.com",
-    messagingSenderId: "867592075139",
-    appId: "1:867592075139:web:bc8cfdcd4d74bd0a2668b6",
-    measurementId: "G-3SEHN30EF8"
+    apiKey: VITE_FIREBASE_API_KEY,
+    authDomain: VITE_FIREBASE_AUTH_DOMAIN,
+    databaseURL: VITE_FIREBASE_DATABASE_URL,
+    projectId: VITE_FIREBASE_PROJECT_ID,
+    storageBucket: VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: VITE_FIREBASE_APP_ID,
+    measurementId: VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
