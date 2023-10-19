b97400259ae5dbfee609d3c19fe0e240f6b9c2d9
Garik Asplund <garik.asplund@gmail.com>
2023-10-18 17:34:28 -0700

    remove RickRoll

--- src/lib/components/Admin.svelte
+++ src/lib/components/Admin.svelte
@@ -20,7 +20,7 @@
             crunchData();
         }
         
-        window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
+        // window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
     }
 
     function crunchData() {
