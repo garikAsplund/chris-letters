9ffc183fa951a0249e7de780c64f605b9b390fbe
Garik Asplund <garik.asplund@gmail.com>
2023-11-10 17:12:01 -0800

    added console log

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,20 @@
     import { blur } from "svelte/transition";
     import { onMount } from 'svelte';
 
+    console.log(`%c
+    ▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
+    ▐ __  __      __       ▌
+    ▐/\\ \\/\\ \\  __/\\ \\      ▌
+    ▐\\ \\ \\_\\ \\/\\_\\ \\ \\     ▌
+    ▐ \\ \\  _  \\/\\ \\ \\ \\    ▌
+    ▐  \\ \\ \\ \\ \\ \\ \\ \\_\\   ▌
+    ▐   \\ \\_\\ \\_\\ \\_\\/\\_\\  ▌
+    ▐    \\/_/\\_\\/_/\\/_/\\_\\ ▌
+    ▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
+    `, 'font-family: "Courier New", monospace; font-size: 14px; color: aqua;');
+
+
+
     let ready = false;
     onMount(() => ready = true);
     
