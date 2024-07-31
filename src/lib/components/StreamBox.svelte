6e7958023dd16d3ab4dfb6c80e8cea70b89c3ba9
Garik Asplund <garik.asplund@gmail.com>
2024-07-31 14:54:04 -0700

    photo madness in prod

--- src/lib/components/StreamBox.svelte
+++ src/lib/components/StreamBox.svelte
@@ -197,7 +197,7 @@
 						const imageName = `${Math.floor(surpriseCount) % 2 == 0 ? 'face' : 'object'}${Math.floor(surpriseCount++ / 2) + 1}`;
     					$everySurprisePath.push(imageName);
     					surprisePath = surpriseImages[imageName];
-						console.log({surprisePath});
+						// console.log({surprisePath});
 					}
 				if ($isTarget) {
 					$targetLetter += $currentLetter;
